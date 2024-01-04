1becf95092248ddb30c22f38127aa07d9ba51d3e
Samuel Lijin <github@sxlijin.com>
2024-01-04 15:19:42 -0800

    commit whitespace change

--- absl/debugging/failure_signal_handler.h
+++ absl/debugging/failure_signal_handler.h
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// whitespace change
+
+
 // -----------------------------------------------------------------------------
 // File: failure_signal_handler.h
 // -----------------------------------------------------------------------------
